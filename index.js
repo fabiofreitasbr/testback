74b9506396e2fc422cfe60793e8ac300e27da0b1
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2023-12-05 03:02:21 -0300

    test

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const app = express()
-const PORT = 5000;
 
 
 app.get('/', (req, res) => {
   res.status(200).json('Olá bem vindo');
 })
-
-
-app.listen(PORT, () => {
-  console.log(`Server running at ${PORT}`);
-});
 
 // Export the Express API
 module.exports = app
